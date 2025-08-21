6d618f74771f0ecbc5bb4933db6f970b3be78b31
gituser <danjee2009@gmail.com>
2025-08-21 19:37:35 +0900

    test

---
+++ test/test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by maple on 8/21/25.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
