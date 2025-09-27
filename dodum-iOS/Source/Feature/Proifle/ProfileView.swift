b6c461d4ca4de209af722733af7e12764a639a67
gituser <danjee2009@gmail.com>
2025-09-27 17:42:28 +0900

    initial Commit

---
+++ dodum-iOS/Source/Feature/Proifle/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  dodum-iOS
+//
+//  Created by maple on 9/27/25.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
